8eed90e67901658885d3c97eb6c95f9860d859aa
Vahid <vsaberi.iust@gmail.com>
2018-02-25 22:41:05 -0800

    code created

--- model.py
+++ model.py
@@ -1,6 +1,3 @@
-import numpy as np
-from scipy.integrate import odeint
-import matplotlib.pyplot as plt
 
 """""
 Author: Vahid Saberi
@@ -8,207 +5,243 @@ Date: Feb 25, 2018
 
 Description:
 
-The code regenerates the simulations of the following paper:
+The purpose of this code is to regenerates the simulations of the following paper:
 
 Mishra, Sharmistha, David N. Fisman, and Marie-Claude Boily.
 "The ABC of terms used in mathematical models of infectious diseases."
 Journal of Epidemiology & Community Health (2010): jech-2009.
-""""
 
+The includes a model of SIRS and SIS structures used for mathematical modeling
+of infectious diseases
 
+"""
 
-def model_A(X,t):
-    S=X[0]
-    I=X[1]
+#import required moduls
 
-    #parameters
-    R0=21
-    r_R=1/70
-
-    N=S+I
-
-    lambda_ = R0 * r_R * I / N
-
-    X_dot=[]
-    X_dot.append(r_R*N-lambda_*S-r_R*S)
-    X_dot.append(lambda_*S-r_R*I)
-
-    return X_dot
-
-def model_B(X,t):
-    S=X[0]
-    I=X[1]
-
-    #parameters
-    R0=3
-    r_R=0
-    r_L=0.1
-
-    X_dot=[]
-    N=S+I
-    lambda_=R0*(r_R+r_L)*I/N
-    X_dot.append(r_R*N-lambda_*S-r_R*S+r_L*I)
-    X_dot.append(lambda_*S-r_R*I-r_L*I)
-
-    return X_dot
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
 
 
 
 
-def model_C(X,t):
-    S=X[0]
-    I=X[1]
-    R=X[2]
+class SIRS_model:
 
-    #parameters
-    R0=3
-    r_R=0
-    r_l=0.1
+    """
+    This class defines a SIRS model for infectious disease modeling.
+    For more info see the above-mentioned paper.
 
-    X_dot=[]
-    N=S+I+R
-    lambda_=R0*(r_R+r_l)*I/N
+    Model parameters:
 
-    X_dot.append(r_R*N-lambda_*S-r_R*S)
-    X_dot.append(lambda_*S-r_R*I-r_l*I)
-    X_dot.append(r_l*I-r_R*R)
+    r_R:        Rate of death
+    r_L:        Rate of loss of immunity
+    r_l:        Rate of developing protective immunity
+    R0:         reproduction ratio
 
+    Model solver inputs:
 
-    return X_dot
-
-
-
-def model_D(X,t):
-    S=X[0]
-    I=X[1]
-    R=X[2]
-
-    #parameters
-    R0=3
-    r_R=0
-    r_l=0.1
-    r_L=0.006
-
-    X_dot=[]
-    N=S+I+R
-    lambda_=R0*(r_R+r_l)*I/N
-
-    X_dot.append(r_R*N-lambda_*S+r_L*R-r_R*S)
-    X_dot.append(lambda_*S-r_R*I-r_l*I)
-    X_dot.append(r_l*I-r_L*R-r_R*R)
+    S0:         Initial population fraction of susceptible hosts
+    I0:         initial population fraction of infectious people
+    R0:         Initial population fraction of recovered people
+    t:          Simulation duration
+    dt:         Time step size
 
 
-    return X_dot
+    Model outputs:
+
+    t:         Time
+    S(t):         Time series of population fraction of susceptible hosts
+    I(t):         Time series of population fraction of infectious people
+    R(t):         Time series of population fraction of recovered people
+
+    """
+
+    def __init__(self,r_R,r_L,r_l,R0):
+
+
+        # parameters
+        self.R0 = R0
+        self.r_R = r_R
+        self.r_l = r_l
+        self.r_L = r_L
+
+
+    def _model(self,X, t):
+
+        """"
+        This function is an internal function that models the dynmaics.
+        It takes the X=[S,I,R] and time vector and returns X_dot.
+        """
+
+        S = X[0]
+        I = X[1]
+        R = X[2]
+
+        X_dot = []
+        N = S + I + R
+        lambda_ = self.R0 * (self.r_R + self.r_l) * I / N
+
+        X_dot.append(self.r_R * N - lambda_ * S + self.r_L * R - self.r_R * S)
+        X_dot.append(lambda_ * S - self.r_R * I - self.r_l * I)
+        X_dot.append(self.r_l * I - self.r_L * R - self.r_R * R)
+
+        return X_dot
+
+    def solve(self,S_0,I_0,R_0,t,dt):
+
+        # initial condition
+        X0 = [S_0, I_0, R_0]
+
+        #time
+        time = np.linspace(0, t, int(1/(dt)))
+
+        # solve ODE
+        X = odeint(self._model, X0, time)
+
+        S = [X[i][0] for i in range(len(X))]
+        I = [X[i][1] for i in range(len(X))]
+        R = [X[i][2] for i in range(len(X))]
+
+        return time,S,I,R
 
 
 
 
-#Solve the models
+class SIS_model:
+
+    """
+    This class defines a SIS model for infectious disease modeling.
+    For more info see the above-mentioned paper.
+
+    Model parameters:
+
+    r_R:        Rate of death
+    r_L:        Rate of loss of immunity
+    R0:         reproduction ratio
+
+    Model solver inputs:
+
+    S0:         Initial population fraction of susceptible hosts
+    I0:         initial population fraction of infectious people
+    t:          Simulation duration
+    dt:         Time step size
+
+
+    Model outputs:
+
+    t:         Time
+    S(t):         Time series of population fraction of susceptible hosts
+    I(t):         Time series of population fraction of infectious people
+
+    """
+
+    def __init__(self,r_R,r_L,R0):
+
+
+        # parameters
+        self.R0 = R0
+        self.r_R = r_R
+        self.r_L = r_L
+
+
+    def _model(self,X, t):
+
+        """"
+        This function is an internal function that models the dynmaics.
+        It takes the X=[S,I] and time vector and returns X_dot.
+        """
+
+        S=X[0]
+        I=X[1]
+
+
+        X_dot = []
+        N = S + I
+        lambda_ = self.R0 * (self.r_R + self.r_L) * I / N
+
+        X_dot.append(self.r_R * N - lambda_ * S + self.r_L * I - self.r_R * S)
+        X_dot.append(lambda_ * S - self.r_R * I - self.r_L * I)
+
+        return X_dot
+
+    def solve(self,S_0,I_0,t,dt):
+
+        # initial condition
+        X0 = [S_0, I_0]
+
+        #time
+        time = np.linspace(0, t, int(1/(dt)))
+
+        # solve ODE
+        X = odeint(self._model, X0, time)
+
+        S = [X[i][0] for i in range(len(X))]
+        I = [X[i][1] for i in range(len(X))]
+
+        return time,S,I
+
+
+
+#plotting tool
+def plot_SIR(time,S=None,I=None,R=None,title='Model (D)',x_label='time (days)',y_label='Population fraction'):
+    # plot results
+    if S is not None:
+        plt.plot(time, S, 'b', label='susceptible')
+    if I is not None:
+        plt.plot(time, I, 'r', label='infectious')
+    if R is not None:
+        plt.plot(time, R, 'g', label='recovered')
+
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.legend()
+    plt.title(title)
+    plt.show()
+
+
+
+
+
+
+
+#Solve the models corresponding to plots A, B, C and D in the paper
 
 def solve_model_A():
 
-    # initial condition
-    X0 =[1-0.001,0.001]
+    model_A=SIRS_model(r_R=1/70,r_L=0,r_l=0,R0=21)
+    time,S,I,_=model_A.solve(S_0=0.999,I_0=0.001,R_0=0,t=600,dt=0.001)
 
-    # time points
-    t = np.linspace(0, 600,10000)
-
-    # solve ODE
-    X = odeint(model_A, X0, t)
-
-    S=[X[i][0] for i in range(len(X))]
-    I=[X[i][1] for i in range(len(X))]
-
-
-    # plot results
-    plt.plot(t,S,'b',label='susceptible')
-    plt.plot(t,I,'r',label='infectious')
-    plt.xlabel('time (days)')
-    plt.ylabel('Population fraction')
-    plt.legend()
-    plt.title('Model (A)')
-    plt.show()
+    plot_SIR(time=time,S=S,I=I,R=None,title='Model (A)')
 
 
 def solve_model_B():
 
-    # initial condition
-    X0 =[1-0.001,0.001]
+    model_B=SIS_model(r_R=0,r_L=0.1,R0=3)
+    time,S,I=model_B.solve(S_0=0.999,I_0=0.001,t=600,dt=0.001)
 
-    # time points
-    t = np.linspace(0, 600,10000)
-
-    # solve ODE
-    X = odeint(model_B, X0, t)
-
-    S=[X[i][0] for i in range(len(X))]
-    I=[X[i][1] for i in range(len(X))]
-
-
-    # plot results
-    plt.plot(t,S,'b',label='susceptible')
-    plt.plot(t,I,'r',label='infectious')
-    plt.xlabel('time (days)')
-    plt.ylabel('Population fraction')
-    plt.legend()
-    plt.title('Model (B)')
-    plt.show()
+    plot_SIR(time=time,S=S,I=I,R=None,title='Model (B)')
 
 
 def solve_model_C():
 
-    # initial condition
-    X0 =[1-0.001,0.001,0]
+    model_C=SIRS_model(r_R=0,r_L=0,r_l=0.1,R0=3)
+    time,S,I,R=model_C.solve(S_0=0.999,I_0=0.001,R_0=0,t=600,dt=0.001)
 
-    # time points
-    t = np.linspace(0, 600,10000)
-
-    # solve ODE
-    X = odeint(model_C, X0, t)
-
-    S=[X[i][0] for i in range(len(X))]
-    I=[X[i][1] for i in range(len(X))]
-    R=[X[i][2] for i in range(len(X))]
-
-
-    # plot results
-    plt.plot(t,S,'b',label='susceptible')
-    plt.plot(t,I,'r',label='infectious')
-    plt.plot(t,R,'g',label='recovered')
-    plt.xlabel('time (days)')
-    plt.ylabel('Population fraction')
-    plt.legend()
-    plt.title('Model (C)')
-    plt.show()
-
-
+    plot_SIR(time=time,S=S,I=I,R=R,title='Model (C)')
 
 def solve_model_D():
 
-    # initial condition
-    X0 =[1-0.001,0.001,0]
+    model_D=SIRS_model(r_R=0,r_L=0.006,r_l=0.1,R0=3)
+    time,S,I,R=model_D.solve(S_0=0.999,I_0=0.001,R_0=0,t=600,dt=0.001)
 
-    # time points
-    t = np.linspace(0, 600,10000)
-
-    # solve ODE
-    X = odeint(model_D, X0, t)
-
-    S=[X[i][0] for i in range(len(X))]
-    I=[X[i][1] for i in range(len(X))]
-    R=[X[i][2] for i in range(len(X))]
+    plot_SIR(time=time,S=S,I=I,R=R,title='Model (D)')
 
 
-    # plot results
-    plt.plot(t,S,'b',label='susceptible')
-    plt.plot(t,I,'r',label='infectious')
-    plt.plot(t,R,'g',label='recovered')
-    plt.xlabel('time (days)')
-    plt.ylabel('Population fraction')
-    plt.legend()
-    plt.title('Model (D)')
-    plt.show()
+
 
 
 if __name__=="__main__":
+    # solve_model_A()
+    # solve_model_B()
+    # solve_model_C()
     solve_model_D()
